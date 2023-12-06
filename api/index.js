073c084c13e7d1a0860db96370e3b148a1f263a4
redgem <redgem@code.com>
2023-12-06 19:00:05 +0530

    mongo url corrected

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ import path from 'path';
 //import cors from 'cors';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect('mongodb+srv://aman:aman@mern-estate.5gckoc3.mongodb.net/mern-estate?retryWrites=true&w=majority').then(()=>{
     console.log('Connected to MongoDB!');
     }).catch((err)=>{
         console.log(err);
